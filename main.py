638125a4be997735e8be71bd69cc29d4d4a61f62
Wen XU <xuwen917@hotmail.com>
2023-04-30 18:47:21 +0200

    Tfdif

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import pickle
+import gensim
 import pandas as pd
 import streamlit as st
 from bs4 import BeautifulSoup
 from sklearn.preprocessing import MultiLabelBinarizer
-from gensim.utils import simple_preprocess
-from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
-import tensorflow_hub as hub
+
+def preprocess(text):
+    result=[]
+    for token in gensim.utils.simple_preprocess(text) :
+        if token not in gensim.parsing.preprocessing.STOPWORDS:
+            result.append(token)
+    return result
 
 # Define the Streamlit app
 st.title('Stack Overflow Question Tagger')
-
-# Load the Universal Sentence Encoder
-use = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 
 y = [['android', 'c', 'c#', 'c++', 'css', 'html', 'ios', 'java', 'javascript', 'jquery', 'net', 'php', 'python', 'r']]
 mlb = MultiLabelBinarizer()
@@ -21,17 +23,17 @@ vectorizer = TfidfVectorizer()
 texte = st.text_area('Enter some text here')
 
 if st.button('Predict the text'):
-    soup = BeautifulSoup(texte, 'html.parser')
-    tokens = simple_preprocess(texte)
-    stop_words = set(stopwords.words('english'))
-    tokens = [token for token in tokens if token not in stop_words]
-    X = [" ".join(tokens)]
-    vectorizer.fit(X)
-    #X_vec = vectorizer.transform(X)
-    X_emb = use(X)
+    soup = texte.apply(lambda x: BeautifulSoup(x, 'html.parser').get_text())
+    tokens = soup.apply(lambda x:preprocess(x))
+    X = tokens.apply(lambda x: " ".join(x))
+    X_vec = vectorizer.fit_transform(X)
+    arr = np.array(X_vec)
+    while arr.size < 28075:
+        arr = np.append(arr, 0)
+    X_vec_new = arr.reshape((1, 28075))
     # importer le modele
-    model = pickle.load(open('../pythonProject3/svc.pkl', 'rb'))
-    prediction = model.predict(X_emb)
+    model = pickle.load(open('../Web_app/svc_v.pkl', 'rb'))
+    prediction = model.predict(X_vec_new)
     tags = mlb.inverse_transform(prediction)
     df_pred = pd.DataFrame(tags)
     st.write(df_pred)
