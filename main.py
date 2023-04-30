6996dbc8ab271a09e4bf13fadf3dbe443180bbcd
wenxxxx <95685464+wenxxxx@users.noreply.github.com>
2023-04-30 22:01:20 +0200

    reset use

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ from bs4 import BeautifulSoup
 from sklearn.preprocessing import MultiLabelBinarizer
 from gensim.utils import simple_preprocess
 from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import TfidfVectorizer
-#import tensorflow_hub as hub
+#from sklearn.feature_extraction.text import TfidfVectorizer
+import tensorflow_hub as hub
 
 # Define the Streamlit app
 st.title('Stack Overflow Question Tagger')
 
 # Load the Universal Sentence Encoder
-#use = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+use = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 
 y = [['android', 'c', 'c#', 'c++', 'css', 'html', 'ios', 'java', 'javascript', 'jquery', 'net', 'php', 'python', 'r']]
 mlb = MultiLabelBinarizer()
 y_ = mlb.fit_transform(y)
-vectorizer = TfidfVectorizer()
+#vectorizer = TfidfVectorizer()
 texte = st.text_area('Enter some text here')
 
 if st.button('Predict the text'):
@@ -26,12 +26,12 @@ if st.button('Predict the text'):
     stop_words = set(stopwords.words('english'))
     tokens = [token for token in tokens if token not in stop_words]
     X = [" ".join(tokens)]
-    vectorizer.fit(X)
-    X_vec = vectorizer.transform(X)
-    #X_emb = use(X)
+    #vectorizer.fit(X)
+    #X_vec = vectorizer.transform(X)
+    X_emb = use(X)
     # importer le modele
-    model = pickle.load(open('../Web_app/svc_v.pkl', 'rb'))
-    prediction = model.predict(X_vec)
+    model = pickle.load(open('../pythonProject3/svc.pkl', 'rb'))
+    prediction = model.predict(X_emb)
     tags = mlb.inverse_transform(prediction)
     df_pred = pd.DataFrame(tags)
     st.write(df_pred)
